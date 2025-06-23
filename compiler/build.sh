0eaa3bf7a3737c04738892360344d0abe0011a48
Hector Cao <hector.cao@canonical.com>
2025-06-23 14:30:40 +0000

    add fetch and build scripts

---
+++ compiler/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# build and install UMF
+(cd unified-memory-framework/ && cmake -DUMF_BUILD_CUDA_PROVIDER=OFF -DUMF_BUILD_TESTS=OFF . && make && make install)
+
+# build and install level-zero v1.21.9
+(cd level-zero/ && cmake . && make && make install)
+
+# SPIRV-Headers (the one in Ubuntu does not work) : 
+(cd SPIRV-Headers/ && cmake -DCMAKE_INSTALL_PREFIX=/usr/ . && make && make install)
+
+rm -rf build
+mkdir -p build/_deps/
+
+cp -rf emhash build/_deps/emhash-headers-src
+cp -rf parallel-hashmap build/_deps/parallel-hashmap-src
+
+#--cmake-opt="-DLLVM_USE_LINKER=mold" \
+#--cmake-opt="-DUR_OPENCL_ICD_LOADER_LIBRARY=OpenCL" \
+python3 ./buildbot/configure.py \
+         --l0-headers /usr/local/include/level_zero/ \
+         --l0-loader /usr/local/lib/libze_loader.so \
+         --cmake-opt="-DLLVM_PARALLEL_LINK_JOBS=4" \
+         --disable-jit \
+         --cmake-opt="-DLLVM_ENABLE_RTTI=ON" \
+         --cmake-opt="-DLLVM_BUILD_LLVM_DYLIB:BOOL=ON" \
+         --cmake-opt="-DSYCL_UR_USE_FETCH_CONTENT=OFF" \
+         --cmake-opt="-DLLVMGenXIntrinsics_SOURCE_DIR=$PWD/vc-intrinsics" \
+         --cmake-opt="-DFETCHCONTENT_FULLY_DISCONNECTED=ON" \
+         --cmake-opt="-DSYCL_UR_SOURCE_DIR=$PWD/unified-runtime" \
+         --cmake-opt="-DUR_OPENCL_INCLUDE_DIR=/usr/include/" \
+         --cmake-opt="-DUR_USE_EXTERNAL_UMF=ON" \
+         --cmake-opt="-DCOMPUTE_RUNTIME_LEVEL_ZERO_INCLUDE=/usr/include/level_zero" \
+         --cmake-opt="-DOpenCL_INCLUDE_DIR=/usr/include/" \
+         --cmake-opt="-DBOOST_MP11_SOURCE_DIR=$PWD/mp11-boost" \
+         --cmake-opt="-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=/usr/include/" \
+
+python3 ./buildbot/compile.py
---
+++ compiler/fetch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+VERSION=6.1.0
+SRC_FOLDER=llvm-${VERSION}
+
+# sycl compiler (llvm)
+if [ ! -d "${SRC_FOLDER}" ]; then
+    git clone -b v${VERSION} https://github.com/intel/llvm.git ${SRC_FOLDER}
+fi
+
+# UMF tag v0.11.2
+git clone -b v0.12.0-dev3 https://github.com/oneapi-src/unified-memory-framework.git ${SRC_FOLDER}/unified-memory-framework/
+
+# unified runtime (on v6.1.0), unified runtime is not bundled into the compiler
+git clone -b v0.11.10 https://github.com/oneapi-src/unified-runtime.git ${SRC_FOLDER}/unified-runtime
+
+# level-zero v1.21.9
+git clone -b v1.21.9 https://github.com/oneapi-src/level-zero.git ${SRC_FOLDER}/level-zero/
+ 
+# SPIRV-Headers (the one in Ubuntu does not work) :
+git clone -b vulkan-sdk-1.4.313.0 https://github.com/KhronosGroup/SPIRV-Headers.git ${SRC_FOLDER}/SPIRV-Headers
+
+# emhash
+git clone https://github.com/ktprime/emhash ${SRC_FOLDER}/emhash
+
+# parallel-hashmap
+git clone -b v2.0.0 https://github.com/greg7mdp/parallel-hashmap.git  ${SRC_FOLDER}/parallel-hashmap
+
+# mp11-boost
+git clone -b boost-1.88.0 https://github.com/boostorg/mp11.git ${SRC_FOLDER}/mp11-boost
+
+# vc-intrinsics
+
+cp build.sh ${SRC_FOLDER}
+cd ${SRC_FOLDER}
+
+
