9453cf6491928b50c7fc61453a9364f17e76df64
Will French <will.french@canonical.com>
2025-08-01 11:29:49 -0500

    Update binary package name, move script

--- emhash/README.md
+++ emhash/README.md
@@ -3,7 +3,7 @@
 To update and build this package:
 
 ```shell
-./build-orig-tarball.sh
+debian/build-origin-tarball.sh
 cd ../emhash-0.0+git20250614.b7ff314/
-sbuild -c plucky-amd64 --dist=plucky
+sbuild -c plucky-amd64 --dist=plucky --build-path=""
 ```
--- emhash/debian/build-orig-tarball.sh
+++ emhash/debian/build-orig-tarball.sh
@@ -5,6 +5,7 @@
 # so this script builds the orig tarball manually.
 #
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 UPSTREAM_COMMIT_HASH="b7ff314"
 
 cleanup() {
@@ -25,11 +26,17 @@ source_dir="emhash-${upstream_version_string}"
 orig_tarball="emhash_${upstream_version_string}.orig.tar.gz"
 popd
 
-pushd ..
+pushd "${SCRIPT_DIR}"/../..
 mkdir "${source_dir}"
 cp "${tmpdir}/emhash/"*.{hpp,h} "${source_dir}"
-tar -czvf "${orig_tarball}" "${source_dir}"
+# TODO: for future releases, update this command to build
+#       the tarball in a reproducible way. Currently this
+#       vanilla approach results in a different md5sum each
+#       time. For now, I will download the orig tarball from
+#       Launchpad so the upload will not fail 
+#tar -czvf "${orig_tarball}" "${source_dir}"
+wget https://launchpad.net/~kobuk-team/+archive/ubuntu/oneapi/+sourcefiles/emhash/0.0+git20250614.b7ff314-0ubuntu1~25.04~ppa1/emhash_0.0+git20250614.b7ff314.orig.tar.gz
 popd
 
 # for convenience, copy the debian folder into the upstream source tree
-cp -r debian ../"${source_dir}"
+cp -r "${SCRIPT_DIR}" "${SCRIPT_DIR}/../../${source_dir}"
