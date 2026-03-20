717409a2cb84b044370137694020ec5fff276dbb
Will French <will.french@canonical.com>
2026-03-20 12:55:33 -0500

    onednn: d/tests: update command and package names.
    * d/tests:
      - Use new names of DPC++ compiler commands.
      - Update DPC++ binary package name in Depends from
      clang-dpcpp-21 to dpclang-6.
      - Drop libsycl-dev and libclang-dpcpp-common-21-dev from
      Depends because these are runtime dependencies for dpclang-6.

--- onednn/debian/tests/testutil.sh
+++ onednn/debian/tests/testutil.sh
@@ -22,7 +22,7 @@ dump_test_command () {
 	local _exe=$(basename ${_src%.*})_tester
 	cat >> debian/tests/control <<EOF
 Test-Command: install -D -m 0644 /usr/lib/onednn/$_src $_src; $_cxx ${cxxflags} ${ldflags} $_src ${libs} -o $_exe; ./$_exe gpu
-Depends: @, onednn-sycl-examples, libtbb-dev, clang-dpcpp-21, libsycl-dev, libclang-dpcpp-common-21-dev, ocl-icd-opencl-dev
+Depends: @, onednn-sycl-examples, libtbb-dev, dpclang-6, ocl-icd-opencl-dev
 Restrictions: allow-stderr
 Architecture: amd64
 Features: test-name=$_cxx-$(echo ${_src} | awk -F/ '{print $NF}')
@@ -59,7 +59,7 @@ examples/graph/sycl_single_op_partition.cpp
 
 case "$1" in
 	test)
-		for compiler in clang++-dpcpp; do
+		for compiler in dpclang++; do
 			for t in ${tests[@]}; do
 				compile_and_test $compiler $t
 			done
@@ -67,7 +67,7 @@ case "$1" in
 		;;
 	generate)
 		truncate -s0 debian/tests/control
-		for compiler in clang++-dpcpp; do
+		for compiler in dpclang++; do
 			for t in ${tests[@]}; do
 				dump_test_command $compiler $t
 			done
